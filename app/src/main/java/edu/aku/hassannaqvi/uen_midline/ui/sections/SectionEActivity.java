9a118e73e6bdba807a7252e30e9e3736c4a293ed
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-20 16:57:24 +0500

    layout done

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionEActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_midline.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.uen_midline.R;
+
+public class SectionEActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_e);
+    }
+}
