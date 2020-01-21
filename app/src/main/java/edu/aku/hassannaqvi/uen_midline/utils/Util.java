b8ccd7ee43d78aca34dd5eadf8f420de64d69897
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-21 12:06:54 +0500

    layout added

--- app/src/main/java/edu/aku/hassannaqvi/uen_midline/utils/Util.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/utils/Util.java
@@ -1,13 +1,20 @@
 package edu.aku.hassannaqvi.uen_midline.utils;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 
 import androidx.core.content.ContextCompat;
 
+import org.json.JSONException;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.aku.hassannaqvi.uen_midline.R;
 
 public class Util {
 
@@ -40,4 +47,37 @@ public class Util {
         }
         return listPermissionsNeeded;
     }
+
+
+    public static void openEndActivity(Activity activity) {
+        new AlertDialog.Builder(activity)
+                .setTitle("END INTERVIEW")
+                .setIcon(R.drawable.ic_power_settings_new_black_24dp)
+                .setCancelable(false)
+                .setCancelable(false)
+                .setMessage("Do you want to End Interview??")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        try {
+//                            SaveDraft();
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                        if (!UpdateDB()) {
+//                            return;
+//                        }
+//                        MainApp.endActivitySetRouting(SectionBActivity.this, SectionBActivity.this, EndingActivity.class, false, null);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .show();
+    }
+
+
 }
