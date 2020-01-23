ce7e3490da4306e493d6c3718a6850c5211de029
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-23 14:07:07 +0500

    routing set

--- app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionH2Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionH2Activity.java
@@ -134,7 +134,7 @@ public class SectionH2Activity extends AppCompatActivity {
             }
             if (UpdateDB()) {
                 finish();
-                startActivity(new Intent(this, SectionI1Activity.class));
+                startActivity(new Intent(this, SectionKActivity.class));
             } else {
                 Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
             }
