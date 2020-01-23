2b7c6daad65af18e7933a83ec38196ed6a8025c3
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-23 11:34:14 +0500

    added section E4

--- app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionE3Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_midline/ui/sections/SectionE3Activity.java
@@ -1,17 +1,61 @@
 package edu.aku.hassannaqvi.uen_midline.ui.sections;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
 
 import edu.aku.hassannaqvi.uen_midline.R;
+import edu.aku.hassannaqvi.uen_midline.core.MainApp;
+import edu.aku.hassannaqvi.uen_midline.databinding.ActivitySectionE3Binding;
 
 public class SectionE3Activity extends AppCompatActivity {
+
+    ActivitySectionE3Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_e3);
 
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_e3);
+        bi.setCallback(this);
+
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                finish();
+                startActivity(new Intent(this, SectionJ02Activity.class));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean formValidation() {
+
+        return Validator.emptyCheckingContainer(this, bi.fldGrpSectionE3);
+    }
+
+    public void BtnEnd() {
+        MainApp.endActivity(this, this);
     }
 }
