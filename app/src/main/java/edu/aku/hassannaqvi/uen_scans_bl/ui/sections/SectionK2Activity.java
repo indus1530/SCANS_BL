74bab1464e7cfc3866c092271ca0414c20bd46f9
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-01-30 13:47:50 +0500

    create layout in section K1, K2 and L.

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_scans_bl/ui/sections/SectionK2Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_scans_bl.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.uen_scans_bl.R;
+
+public class SectionK2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_k2);
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_scans_bl/ui/sections/SectionLActivity.java
@@ -0,0 +1,19 @@
+package edu.aku.hassannaqvi.uen_scans_bl.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.uen_scans_bl.R;
+import edu.aku.hassannaqvi.uen_scans_bl.databinding.ActivitySectionLBinding;
+
+public class SectionLActivity extends AppCompatActivity {
+
+    ActivitySectionLBinding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_l);
+    }
+}
