da492b2f23999012f99391b099db5a077bd4e723
SAJID LATIF <indus1530@gmail.com>
2020-02-03 09:50:09 +0500

    Sections

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_scans_bl/ui/sections/SectionD2Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_scans_bl.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_scans_bl.R;
+
+public class SectionD2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_d2);
+    }
+}
